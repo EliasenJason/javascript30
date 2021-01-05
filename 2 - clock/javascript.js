9faa9a327963a6545a5782103df99425d66d1768
EliasenJason <eliasenjason@gmail.com>
2021-01-04 22:12:43 -0700

    Clock is functioning

--- 2 - clock/javascript.js
+++ 2 - clock/javascript.js
@@ -2,9 +2,11 @@
 function adjustTime() {
     let date = new Date()
     let digital = document.querySelector('#digital p')
-    digital.innerHTML = `${date.getHours()}:${date.getMinutes()}.${date.getSeconds() < 10 ? 0 + date.getSeconds().toString() : date.getSeconds()}`
+    digital.innerHTML = `${date.getHours()}:${date.getMinutes() < 10 ? 0 + date.getMinutes().toString() : date.getMinutes()}.${date.getSeconds() < 10 ? 0 + date.getSeconds().toString() : date.getSeconds()}`
+    document.getElementById('hours').style.transform = `rotate(${date.getHours() > 12 ? (date.getHours() / 12) * 360 : (((date.getHours() - 12) / 12) * 360)}deg`
+    document.getElementById('minutes').style.transform = `rotate(${(date.getMinutes() / 60) * 360}deg)`
+    document.getElementById('seconds').style.transform = `rotate(${(date.getSeconds() / 60) * 360}deg)`
 }
-
 
 setInterval(() => {
     adjustTime()
